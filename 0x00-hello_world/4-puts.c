715d58605f047b5e1aea7c3a31e578d70ef15848
Sheyyi <oluseyialagbe@gmail.com>
2023-07-13 21:52:41 -0700

    Add a script to 4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/**
+ * main - Prints a string of characters
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char Mystr[] = "Programming is like building a multilingual puzzle";
+
+	puts(Mystr);
+
+	return (0);
+}
