e5cdbde662fb0680347596754521ff9ef9cc7bed
Sheyyi <oluseyialagbe@gmail.com>
2023-07-21 18:09:54 -0700

    Add script to 10

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * print_triangle - Prints a triangle followed by a new line.
+ *
+ * @size: THe size of the triangle as an integer.
+ *
+ * Return: 0.
+ */
+void print_triangle(int size)
+{
+	int i, s, h;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		i = 1;
+		while (i <= size)
+		{
+			s = size - 1;
+			while (s > 0)
+			{
+				_putchar(' ');
+				s--;
+			}
+			h = 1;
+			while (h <= i)
+			{
+				_putchar('#');
+				h++;
+			}
+			_putchar('\n');
+			i++;
+		}
+	}
+}
