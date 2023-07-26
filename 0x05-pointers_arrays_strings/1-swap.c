84c65e5bd2d1bfa9c95be81fbd90be15e79fe535
Sheyyi <oluseyialagbe@gmail.com>
2023-07-25 21:22:37 -0700

    Add script to 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap_int - Swaps the values of two integers
+ * @a: A pointer to the integer a
+ * @b: A pointer to the integer b
+ *
+ * Return: nothing.
+ */
+void swap_int(int *a, int *b)
+{
+	int n;
+
+	n = *a;
+	*a = *b;
+	*b = m;
+}
