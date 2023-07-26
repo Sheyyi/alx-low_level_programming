65321d29256ec37f7b4601c1aee8226d25c69834
Sheyyi <oluseyialagbe@gmail.com>
2023-07-25 22:37:39 -0700

    Fix error in 1

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_rev - Prints a string in reverse followed by a new line
+ * @s: Character pointer that points to s
+ *
+ * Return: nothing
+ */
+void print_rev(char *s)
+{
+	int n, l;
+
+	n = 0;
+	l = _strlen(
