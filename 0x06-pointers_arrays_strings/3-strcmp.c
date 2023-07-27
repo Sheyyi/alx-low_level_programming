b633ab68c991846b676e0cade8f5846fde14b230
Sheyyi <oluseyialagbe@gmail.com>
2023-07-27 14:24:39 -0700

    Fix error in 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,17 +10,15 @@
 int _strcmp(char *s1, char *s2)
 {
 	int a;
-	int d;
 
 	a = 0;
-	d = 0;
 	while (s1[a] != '\0' && s2[a] != '\0')
 	{
 		if (s1[a] != s2[a])
 		{
-			d = d + (s1[a] - s2[a]);
+			return (s1[a] - s2[a]);
 		}
 		a++;
 	}
-	return (d);
+	return (0);
 }
