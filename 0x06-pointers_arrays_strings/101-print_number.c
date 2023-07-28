7112ef79ec8c40d75d09ef07e89225ee931a8dd0
Sheyyi <oluseyialagbe@gmail.com>
2023-07-28 15:33:39 -0700

    Fix error in 101

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -8,17 +8,18 @@
  */
 void print_number(int n)
 {
-	int i;
+	unsigned int i;
 
 	i = n;
-	if (i < 0)
+	if (n < 0)
 	{
 		_putchar('-');
 		i = -n;
 	}
+
 	if (i / 10 != 0)
 	{
-		print_number(i /10);
+		print_number(i / 10);
 	}
-	_putchar((i %10) + '0');
+	_putchar((i % 10) + '0');
 }
