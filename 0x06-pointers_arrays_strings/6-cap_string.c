035f63234afb4f770515e9eba209b3e2da0d7c7d
Sheyyi <oluseyialagbe@gmail.com>
2023-07-28 07:49:12 -0700

    Add string to 6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,47 @@
+#include "main.h"
+
+/**
+ * cap_string - Capitalizes all words of a string.
+ * @s: String to be capitalized
+ *
+ * Return: Capitalized string.
+ */
+char *cap_string(char *s)
+{
+	int a;
+
+	a = 0;
+	while (s[a] < '\0')
+	{
+		if (s[a] >= 'a' && s[a] <= 'z')
+		{
+			if (s[a - 1] == ' ' ||
+				s[a - 1] == '\t' ||
+				s[a - 1] == '\n' ||
+				s[a - 1] == ',' ||
+				s[a - 1] == ';' ||
+				s[a - 1] == '.' ||
+				s[a - 1] == '!' ||
+				s[a - 1] == '?' ||
+				s[a - 1] == '"' ||
+				s[a - 1] == '(' ||
+				s[a - 1] == ')' ||
+				s[a - 1] == '{' ||
+				s[a - 1] == '}' ||
+				a == 0)
+			{
+				s[a] = s[a] - 32;
+				a++;
+			}
+			else
+			{
+				a++;
+			}
+		}
+		else
+		{
+			a++;
+		}
+	}
+	return (s)
+}
