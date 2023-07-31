6a215b4d9281c3e95e6e40428944b55eee607b8b
Sheyyi <oluseyialagbe@gmail.com>
2023-07-31 06:18:05 -0700

    Add a script to 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strchr - Locates a character in a string
+ * @s: Address of memory to search
+ * @c: Chracter to search for
+ *
+ * Return: pointer to first occurrence of c.
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+		{
+			return (s[i]);
+		}
+		i++;
+	}
+	return (NULL);
+}
