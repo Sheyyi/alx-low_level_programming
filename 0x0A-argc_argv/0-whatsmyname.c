d9794b2898d6b2784c6c1c3b0363fac2a0c32014
Sheyyi <oluseyialagbe@gmail.com>
2023-08-04 16:41:23 -0700

    Add script to 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Prints the programs name
+ * @argc: The number of arguments passed
+ * @argv: An array of the arguments passed as strings
+ *
+ * Return: 0
+ */
+int main(int argc, char **argv)
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
