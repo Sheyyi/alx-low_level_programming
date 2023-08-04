d802337fdea5acf150fdca0134e752c57ef2c22d
Sheyyi <oluseyialagbe@gmail.com>
2023-08-04 13:47:59 -0700

    Add string to 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * rev_string - Reverses a string
+ * @s: Address of string to be reversed
+ *
+ * Return: 0
+ */
+void rev_string(char *s)
+{
+	char r = s[0];
+	int l, n;
+
+	l = 0;
+	n = 0;
+	while (s[l] != '\0')
+	{
+		l++;
+	}
+	while (n < l)
+	{
+		l--;
+		r = s[n];
+		s[n] = s[l];
+		s[l] = r;
+	}
+}
