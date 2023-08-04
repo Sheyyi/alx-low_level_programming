dc0a63c913f052e7f77246daa02f94b3ce0d0145
Sheyyi <oluseyialagbe@gmail.com>
2023-08-04 14:25:08 -0700

    Add script to 8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_array - Print elements of an array
+ * @a: Address of array to be printed
+ * @n: Number of elements to be printed
+ *
+ * Return: 0
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	i = 0;
+	while (i < (n - 1))
+	{
+		printf("%d, ", a[i]);
+		i++
+	}
+	printf("%d\n", a[n - 1]);
+}
