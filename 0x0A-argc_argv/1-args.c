80ee69691f4877186b03eaa50c8e41ea8a95052a
Sheyyi <oluseyialagbe@gmail.com>
2023-08-04 16:52:08 -0700

    Add script to 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Prints the number of arguments passed into it
+ * @argc: The number of arguments passed
+ * @argv: An array of the arguments passed as strings
+ *
+ * Return: 0
+ */
+int main(int argc, char **argv)
+{
+	(void) argv;
+	printf("%d\n", argc);
+
+	return (0);
+}
