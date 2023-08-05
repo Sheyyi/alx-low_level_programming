c141652185597d28e9242bab6932905207c09429
Sheyyi <oluseyialagbe@gmail.com>
2023-08-04 18:10:18 -0700

    Fix errors in 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,6 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 /**
  * is_number - Checks that a string is a number
@@ -32,7 +33,7 @@ int main(int argc, char **argv)
 	int sum, i, n;
 
 	sum = 0;
-	for (i = 1; i < argc; 1++)
+	for (i = 1; i < argc; i++)
 	{
 		if (is_number(argv[i]))
 		{
