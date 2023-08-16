940cb596bc012ec948e051009f68536bd9ec9be7
Sheyyi <oluseyialagbe@gmail.com>
2023-08-16 02:53:07 -0700

    Add sript to 1

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+#include <stdlib.h>
 
 /**
  * init_dog - Initialize struct dog
@@ -11,10 +12,7 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d = malloc(sizeof(stuct dog));
-	if (d == NULL)
-		return (NULL);
 	d->name = name;
-	*d.age = age;
+	d->age = age;
 	d->owner = owner;
 }
