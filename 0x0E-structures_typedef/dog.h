538ae19d487c99d2576ea3add5b2f9c172ae5f3f
Sheyyi <oluseyialagbe@gmail.com>
2023-08-15 21:55:41 -0700

    Add dir 0x0E and script to 1

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a dog's basic info
+ * @name: First member of type char
+ * @age: Second member of type float
+ * @owner: Third member of type char
+ *
+ * Description: Makes a struct for a dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif
