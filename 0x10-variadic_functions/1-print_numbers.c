b37d7604385e01a069a76dceac9b8cefd5811305
Sheyyi <oluseyialagbe@gmail.com>
2023-08-17 10:35:23 -0700

    Fix error in 1s

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -10,7 +10,7 @@
  *
  * Return: nothing.
  */
-void print_numbers(const char *separator,const unsigned int n, ...)
+void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	va_list ap;
 	unsigned int i;
