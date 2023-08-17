9dd91a7ff1f61b0d3480fd271aea22179f5403b1
Sheyyi <oluseyialagbe@gmail.com>
2023-08-16 21:46:20 -0700

    Add script to 2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -12,5 +12,17 @@
 
 int int_index(int *array, int size, int (*cmp)(int))
 {
+	int i;
 
+	if (array == NULL || size <= 0 || cmp == NULL)
+		return (-1);
+
+	i = 0;
+	while (i < size)
+	{
+		if (cmp(*(array + i)) != 0)
+			return (i);
+		i++;
+	}
+	return (-1);
 }
