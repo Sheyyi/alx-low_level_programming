c98023ef9d3a19c52350136c3fff59b83c6bd08d
Sheyyi <oluseyialagbe@gmail.com>
2023-08-16 21:40:38 -0700

    Fix errors in 0 and 1

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,16 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+
+/**
+ * int_index - Searches for an integer
+ * @array: Array to be searched
+ * @size: number or elements in the array
+ * @cmp:Pointer to function that compares values
+ *
+ * Return: Index of element for which cmp does not return 0, -1 otherwise
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+
+}
