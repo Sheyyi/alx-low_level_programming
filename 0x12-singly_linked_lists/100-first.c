fdf9a7d10ea5f6334861dc52dddf95de7fe9fb8a
Sheyyi <oluseyialagbe@gmail.com>
2023-08-25 02:33:56 -0700

    Add script to 100

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * startup_code - Executes code before main function
+ *
+ * Return: Nothing
+ */
+void startup_code(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
