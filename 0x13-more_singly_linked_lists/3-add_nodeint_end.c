10a013d37350a30188407fb061a7a08369431f69
Sheyyi <oluseyialagbe@gmail.com>
2023-08-29 15:44:39 -0700

    Add script to 3

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+
+/**
+ * add_nodeint_end - Adds a new node at the end of a linked list
+ * @head:
+ * @n: Integer to be added
+ *
+ * Return: Address of the new element, NULL otherwise
+ */
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *lk;
+	listint_t *linkedlist;
+
+	lk = malloc(sizeof(listint_t));
+	if (lk == NULL)
+		return (NULL);
+
+	lk->n = n;
+	linkedlist = *head;
+
+	if (*head == NULL)
+	{
+		*head = lk;
+		return (lk);
+	}
+
+	while (linkedlist->next != NULL)
+		linkedlist = linkedlist->next;
+
+	linkedlist->next = lk;
+
+	return (lk);
+}
