709ce5896cb2cbb41338682521fcb1d596fcef90
Sheyyi <oluseyialagbe@gmail.com>
2023-08-29 16:19:12 -0700

    Add script to 4

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_listint - Frees a linked list from memory
+ * @head: Pointer to head of linked list
+ *
+ * Return: Nothing
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *linkedlist;
+
+	while (head != NULL)
+	{
+		linkedlist = head->next;
+		free(head->n);
+		free(head);
+		head = linkedlist;
+	}
+}
